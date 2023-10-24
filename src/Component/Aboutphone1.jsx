c382a57076d2f31effa9bb962025af131c193409
Aayu0981 <143009935+Aayu0981@users.noreply.github.com>
2023-10-24 12:37:34 +0530

    Update Aboutphone1.jsx

--- src/Component/Aboutphone1.jsx
+++ src/Component/Aboutphone1.jsx
@@ -1,108 +1,85 @@
 import React from 'react'
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaAirbnb, FaArrowLeft, FaFontAwesome, FaFontAwesomeLogoFull, FaPlane } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function Airoplane() {
+
+
+  const navigate = useNavigate();
+  const BASE_URL = 'http://localhost:4000';
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/getUserData`);
+      setIsAirplaneModeOn(response.data.airplanemode);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [isAirplaneModeOn1, setIsAirplaneModeOn] = useState(false);
+  const toggleAirplaneMode = async () => {
+    setIsAirplaneModeOn(!isAirplaneModeOn1);
+
+  }
+
+  useEffect(() => {
+
+    const post = async () => {
+      try {
+        const response = await axios.post(`${BASE_URL}/airplanemodetogle`, {
+          tog: `${isAirplaneModeOn1}`
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error toggling airplane mode', error.message);
+      }
+    };
+
+    post();
+
+
+  }, [isAirplaneModeOn1])
 
 
 
-import { Navigate, isRouteErrorResponse, useNavigate } from 'react-router-dom';
 
 
-function Aboutphone1() {
-  const nevigate = useNavigate()
   return (
-    
-   <>
+    <div style={{ width: 350 }} >
 
-         <div style={{marginLeft:0}} onClick={()=> nevigate(-1)}>
-            <p className='back'><FaArrowLeft/></p>
-        </div>
+      <div onClick={() => navigate(-1)}>
+        <p><FaArrowLeft /></p>
+      </div>
 
-   <p style={{marginLeft:30}} className='heading'>About phone </p>
+      <p className='heading' style={{ marginLeft: 30 }}>Aeroplane Mode</p>
 
+      <div className='Media' style={{ justifyContent: 'space-between' }}>
+        <p className='Darktext1' style={{ marginLeft: 30 }}>Aeroplane Mode</p>
+        <label className="switch">
+          <input type="checkbox" onChange={toggleAirplaneMode} checked={isAirplaneModeOn1} />
+          <span className="slider round"></span>
+        </label>
 
-  
-    <div style={{position:"relative",zIndex:1,display:"flex"}}>
-    <div id='left' style={{width:200,height:300,borderRadius:20,position:"relative"}}>
-    <img src="https://i.imgur.com/G9wfOYe.png"  style={{backgroundColor:"transparent" ,width:100}} />
-     <br />
-     <span >MIUI version</span><br/>
-     <p style={{fontSize:12,color:'grey'}}>MIUI Global</p>
-     <p style={{fontSize:12,marginTop:0,color:'grey'}}>13.0.5</p>
-     <p  style={{fontSize:12,color:'grey',marginTop:0 }}>Stable</p>
-     <p className='up' >Update</p>
-     </div>
-
-     
-
-     <div id='right' >
-      <div className='device' style={{width:170,height:100,borderRadius:20, position: 'relative', zIndex:2}}>
-      <p>Device name</p>
-      <p style={{fontSize:12,color:'grey'}}>POCO M4 Pro</p>
-     </div>
-
-     <div  className='storage' style={{width:180,height:155,borderRadius:20, zIndex:2}}>
-      <p className='s1' >Storage</p><br/>
-      <p className='ooccu' style={{fontSize:12,color:'grey',marginTop:50}}>Occupied</p>
-      <p className='oc1'>59.1 GB<sub>/64 GB</sub></p>
-     </div>
-     </div>
-     </div>
-
-     <div id='MIUI'>
-      <div className="config">
-      <p className='Darktext1'  >MIUI version(For POCO)</p>
-      <p className='about10'>MIUI Global 13.0.5 Stable</p></div>
-      
-     
-      <div className="config"><p className='Darktext1'>Android Version</p>
-      <p className='about10'>12 SP1A.21082.016</p></div>
-      
-      
-      <div className="config"><p className='Darktext1'>Android security update</p>
-      <p className='about10'>2023-04-01</p></div>
-      
-     
-      <div className="config"   onClick={()=>nevigate("/About2")}><p className='Darktext1'>All specs</p>
-      <p className='arow'>&gt;</p></div>
-      
-     </div>
-     <hr/>
-
-     <div >
-      <div className='config' onClick={()=>nevigate("/About3")}><p className='Darktext1'>Back up and restore</p>
-      <p className='arow'>&gt;</p></div>
-      
-      
-      <div className="config"  onClick={()=>nevigate("/About4")}><p className='Darktext1'>Factory reset</p>
-      <p className='arow'>&gt;</p></div>
-     
-      
-     </div>
-
-        <hr/>
-
-     <div>
-      <div className="config"  onClick={()=>nevigate("/About6")}><p className='Darktext1' >Important safety information</p>
-      <p className='arow'>&gt;</p></div>
-      
-      <div className="config"  onClick={()=>nevigate("/About7")}><p className='Darktext1'>certification</p>
-      <span className='arow'>&gt;</span></div>
-      
-     </div>
-  
-
-
-
-     
+      </div>
 
 
 
 
-    
-    </>
-    
-     
 
+
+    </div>
   )
 }
 
-export default Aboutphone1;
+export default Airoplane;
+
+
+
