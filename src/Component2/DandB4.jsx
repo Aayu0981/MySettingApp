8608462b48640e62d407a5bf910bd27cf88469f6
Aayu0981 <143009935+Aayu0981@users.noreply.github.com>
2023-10-25 10:41:42 +0530

    Update DandB4.jsx

--- src/Component2/DandB4.jsx
+++ src/Component2/DandB4.jsx
@@ -1,11 +1,54 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FaArrowLeft } from 'react-icons/fa';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function DandB4() {
 
   const nevigate=useNavigate();
+
+  const BASE_URL = 'http://localhost:4000';
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/getUserData`);
+      setshade(response.data.display.vrmode);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+
+  const [vrMode, setVrMode] = useState("Reduce Blur");
+  const togleToReduceFlicker = async () => {
+    setVrMode("Reduce Flicker");
+  }
+  const togleToReduceBlur = async () => {
+    setVrMode("Reduce Blur");
+  }
+
+  useEffect(() => {
+    const post = async () => {
+      try {
+        const response = await axios.post(`${BASE_URL}/vrmodetogle`, {
+          tog: `${vrMode}`
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error toggling airplane mode', error.message);
+      }
+
+    }
+    post();
+    
+  }, [vrMode]);
+
   return (
     <div  style={{width:350}}>
 
@@ -16,14 +59,14 @@ function DandB4() {
         
         <p className='heading' style={{marginLeft:30}}>When device is in VR</p>
 
-        <div style={{borderRadius:30,backgroundColor:'grey',width:300,paddingTop:5,paddingBottom:5,textAlign:'center'}}>
+        <div  onClick={togleToReduceBlur} className={vrMode==="Reduce Blur"?'compactive':"comp"}>
             <p>Reduce blur (recommended)</p>
         </div>
 
 
 
         
-        <div style={{borderRadius:30,backgroundColor:'grey',width:300,paddingTop:5,paddingBottom:5,textAlign:'center',marginTop:30}}>
+        <div style={{marginTop:30}} className={vrMode==="Reduce Flicker"?'compactive':"comp"} onClick={togleToReduceFlicker}>
             <p>Reduce flicker</p>
         </div>
 
