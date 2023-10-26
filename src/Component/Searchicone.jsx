9c978fd5a637d700cdc97abf8eb6630654e79bfd
Aayu0981 <ayushitomarst4@gmail.com>
2023-10-27 00:11:16 +0530

    Project

--- src/Component/Searchicone.jsx
+++ src/Component/Searchicone.jsx
@@ -1,8 +1,67 @@
+// import React from 'react'
+// import { FaPhone, FaSearch } from 'react-icons/fa'; // Example using Font Awesome
+
+
+// function searchicone() {
+//   return (
+//     <div>
+//  <h1>Settings</h1>
+
+//  <div id='box1' >
+//   <label className="input-label">
+//     <span className="search-icon" >&#128269;</span>
+
+//     <input id='searchbox' type="text" placeholder="Search settings" />
+//   </label>
+// </div>
+
+   
+
+
+//     </div>
+//   )
+// }
+
+// export default searchicone;
+
+
+
 import React from 'react'
 import { FaPhone, FaSearch } from 'react-icons/fa'; // Example using Font Awesome
+import { useState } from 'react';
 
 
-function searchicone() {
+function SearchIcone() {
+
+  const initialSettings = [
+    'About phone',
+    'System aaps updater',
+    'Security status',
+    'Wi-Fi',
+    'Bluetooth',
+    'Sound & vibrartion',
+    'Notifications & control centre',
+    'Wallpaper',
+    'Hotspot'
+  ];
+
+
+  const [searchText, setSearchText] = useState('');
+  const [filteredSettings, setFilteredSettings] = useState(initialSettings);
+
+  const handleSearch = (text) => {
+    const filtered = initialSettings.filter((setting) =>
+      setting.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredSettings(filtered);
+  };
+
+  const handleSearchInputChange = (e) => {
+    const text = e.target.value;
+    setSearchText(text);
+    handleSearch(text);
+  };
+
   return (
     <div>
  <h1>Settings</h1>
@@ -15,6 +74,14 @@ function searchicone() {
   </label>
 </div>
 
+
+
+<ul>
+        {filteredSettings.map((setting, index) => (
+          <li key={index}>{setting}</li>
+        ))}
+      </ul>
+
    
 
 
@@ -22,4 +89,4 @@ function searchicone() {
   )
 }
 
-export default searchicone;
+export default SearchIcone;
